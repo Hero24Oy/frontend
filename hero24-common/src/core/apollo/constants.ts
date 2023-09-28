7c4a27318f9bd43a0d73a42943bb9aa4227cf41b
Maksym Pryhoda <maxum23025@gmail.com>
2023-09-28 17:40:06 +0300

    Extract apollo config variables.

--- hero24-common/src/core/apollo/constants.ts
+++ hero24-common/src/core/apollo/constants.ts
@@ -4,9 +4,6 @@ export const DEFAULT_INPUT_NAME = 'input';
 export const OPERATION_DEFINITION = 'OperationDefinition';
 export const SUBSCRIPTION = 'subscription';
 
-export const BATCH_MAX = 5;
-export const BATCH_INTERVAL = 20;
-
 export enum SortOrder {
   ASC = 'ASC',
   DESC = 'DESC',
