e6d0e2ba0950899f4a52e832be2116319980677c
Maksym Pryhoda <maxum23025@gmail.com>
2023-10-02 17:28:28 +0300

    Remove redundant getOfferStatus mapper.

--- hero24-common/src/modules/Offers/utils.ts
+++
@@ -1,6 +0,0 @@
-import { OFFER_STATUS } from 'hero24-types';
-
-import { GraphQLOfferStatus } from './graphql/constants';
-
-export const getOfferStatus = (status: OFFER_STATUS): GraphQLOfferStatus =>
-  GraphQLOfferStatus[status.toUpperCase() as keyof typeof GraphQLOfferStatus];
