6f5c061027bd75a9f7d4165b064da2d9d2f15947
Maksym Pryhoda <maxum23025@gmail.com>
2023-10-04 13:03:44 +0300

    Remove sagas.

--- hero24-common/src/modules/User/sagas/updateUser/watcher.ts
+++
@@ -1,9 +0,0 @@
-// import { takeEvery } from 'redux-saga/effects';
-
-// import { USER_UPDATE } from '../../actionTypes';
-
-// import { updateUserWorker } from './workers';
-
-export function* updateUserWatcher(): Generator {
-  // yield takeEvery(USER_UPDATE, updateUserWorker);
-}
