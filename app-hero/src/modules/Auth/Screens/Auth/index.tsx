f2d773f0ceb88841794fd65d70088ff78582ae66
Dmytro Laikov <dmytro.laikov@sysgears.com>
2023-10-05 17:40:19 +0300

    Remove toasts. Disable unused buttons

--- app-hero/src/modules/Auth/Screens/Auth/index.tsx
+++ app-hero/src/modules/Auth/Screens/Auth/index.tsx
@@ -10,44 +10,10 @@ import {
   Input,
   InputField,
   Text,
-  Toast,
-  useToast,
   VStack,
 } from '$ui-library';
 
 export const AuthScreen: FC = () => {
-  const toast = useToast();
-
-  const showToast = (description?: string, title?: string): void => {
-    toast.show({
-      placement: 'top',
-      duration: 2500,
-      render() {
-        return (
-          <Toast
-            title={title}
-            description={description}
-            action="error"
-            variant="accent"
-          />
-        );
-      },
-    });
-  };
-
-  const continueWithGoogleOnPress = (): void =>
-    showToast('Login with Google is under construction');
-
-  const continueWithFacebookOnPress = (): void =>
-    showToast('Login with Facebook is under construction');
-
-  const continueWithAppleOnPress = (): void =>
-    showToast('Login with Apple is under construction');
-
-  const continueOnPressHandler = (): void => {
-    console.warn('Continue pressed');
-  };
-
   return (
     <SafeAreaView style={styles.wrapper}>
       <VStack style={styles.container} space="md">
@@ -57,23 +23,19 @@ export const AuthScreen: FC = () => {
         <Input isDisabled={false} isInvalid={false} isReadOnly={false}>
           <InputField placeholder="Password" type="password" />
         </Input>
-        <Button
-          variant="solid"
-          style={styles.continueButton}
-          onPress={continueOnPressHandler}
-        >
+        <Button variant="solid" style={styles.continueButton} disabled>
           <ButtonText size="md">Continue</ButtonText>
         </Button>
         <HStack height={64} alignItems="center" justifyContent="center">
           <Text>Or</Text>
         </HStack>
-        <Button variant="outline" onPress={continueWithGoogleOnPress}>
+        <Button variant="outline" disabled>
           <ButtonText size="md">Continue with Google</ButtonText>
         </Button>
-        <Button variant="outline" onPress={continueWithFacebookOnPress}>
+        <Button variant="outline" disabled>
           <ButtonText size="md">Continue with Facebook</ButtonText>
         </Button>
-        <Button variant="outline" onPress={continueWithAppleOnPress}>
+        <Button variant="outline" disabled>
           <ButtonText size="md">Continue with Apple</ButtonText>
         </Button>
       </VStack>
