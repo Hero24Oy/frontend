fff75cd8984d95cf24b7873a08971b0213bd9446
Dmytro Laikov <dmytro.laikov@sysgears.com>
2023-10-06 17:16:24 +0300

    Remove unused files

--- hero24-ui-library/src/hooks/useToast.tsx
+++
@@ -1,5 +0,0 @@
-import { useToast as useToastOrigin } from '@gluestack-ui/themed';
-
-export const useToast = (): ReturnType<typeof useToastOrigin> => {
-  return useToastOrigin();
-};
--- hero24-ui-library/src/index.ts
+++ hero24-ui-library/src/index.ts
@@ -1,4 +1,3 @@
 export * from './providers';
 export * from './components';
-export * from './hooks';
 export * from './types';
