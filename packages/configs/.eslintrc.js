ee0b9e09c94f84a8aa853d947fb5b308fed07093
Maksym Pryhoda <maxum23025@gmail.com>
2023-10-19 20:48:02 +0300

    Temporary fix instead of eslint.

--- packages/configs/.eslintrc.js
+++ packages/configs/.eslintrc.js
@@ -1,4 +1,4 @@
-const EXCLUDE_NAMES_NAMING_CONVENTION_WORDS = ['heroBIOText', 'Gluestack'];
+const EXCLUDE_NAMES_NAMING_CONVENTION_WORDS = ['heroBIOText', 'Gluestack', 'HStack','VStack'];
 const EXCLUDE_NAMES_NAMING_CONVENTION_REGEXPS = [
   '.*VAT.*',
   '.*URL.*',
