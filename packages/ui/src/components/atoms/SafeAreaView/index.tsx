7310f4aa49345ab909b7813c4a128d23c1a23447
Maksym Pryhoda <maxum23025@gmail.com>
2023-10-19 12:03:27 +0300

    Stick to atoms approach.

---
+++ packages/ui/src/components/atoms/SafeAreaView/index.tsx
@@ -0,0 +1,17 @@
+// import { FC, PropsWithChildren } from 'react'; // TODO fix this
+// import { StyleSheet } from 'react-native';
+// import { SafeAreaView as RnSafeAreaView } from 'react-native-safe-area-context';
+
+// interface Props extends PropsWithChildren {}
+
+// export const SafeAreaView: FC<Props> = (props) => {
+//   const { children } = props;
+
+//   return <RnSafeAreaView style={styles.view}>{children}</RnSafeAreaView>;
+// };
+
+// const styles = StyleSheet.create({
+//   view: {
+//     flex: 1,
+//   },
+// });
---
+++ packages/ui/src/components/atoms/index.ts
@@ -0,0 +1,6 @@
+export * from './Button';
+export * from './Input';
+export * from './Text';
+export * from './VStack';
+export * from './HStack';
+// export * from './SafeAreaView'; // TODO react native is not being installed
