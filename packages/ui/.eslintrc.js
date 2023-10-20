36dccfb9af2edf422861212f8041302d576cdfb1
Andrii Zinko <andrii.zinko@sysgears.com>
2023-10-20 17:49:51 +0300

    Add cspell config and eslint override rule.

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,7 +1,17 @@
 const commonEslint = require('@hero24/configs/.eslintrc');
 
+const overrides = {
+  noMagicNumbersExceptions: {
+    files: ['src/configs/*.ts'],
+    rules: {
+      'no-magic-numbers': 'off',
+    },
+  },
+}
+
 module.exports = {
   ...commonEslint,
+  overrides: commonEslint.overrides.concat(Object.values(overrides)),
   parserOptions: {
     ...commonEslint.parserOptions,
     tsconfigRootDir: __dirname,
