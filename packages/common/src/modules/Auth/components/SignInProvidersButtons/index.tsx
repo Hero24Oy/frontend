b6478ef9c80b29ad625fcd74a572839c2a849109
Andrii Zinko <andrii.zinko@sysgears.com>
2023-10-20 18:07:36 +0300

    Lint files and update package.lock.

--- packages/common/src/modules/Auth/components/SignInProvidersButtons/index.tsx
+++ packages/common/src/modules/Auth/components/SignInProvidersButtons/index.tsx
@@ -7,9 +7,15 @@ export const SignInProvidersButtons: FC = () => {
   return (
     <VStack style={styles.container}>
       {/* TODO replace buttons labels with i18n call */}
-      <Button variant="outline" size="medium" isDisabled>Continue with Google</Button>
-      <Button variant="outline" size="medium" isDisabled>Continue with Facebook</Button>
-      <Button variant="outline" size="medium" isDisabled>Continue with Apple</Button>
+      <Button variant="outline" size="medium" isDisabled>
+        Continue with Google
+      </Button>
+      <Button variant="outline" size="medium" isDisabled>
+        Continue with Facebook
+      </Button>
+      <Button variant="outline" size="medium" isDisabled>
+        Continue with Apple
+      </Button>
     </VStack>
   );
 };
