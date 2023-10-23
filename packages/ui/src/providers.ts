f478087eef7dea6ab21a1993bfaa450c242cf06a
Andrii Zinko <andrii.zinko@sysgears.com>
2023-10-24 01:12:07 +0300

    Update provider.

--- packages/ui/src/providers.ts
+++ packages/ui/src/providers.ts
@@ -1,7 +1,8 @@
-import { config } from '@gluestack-ui/config';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { CombineProviders } from 'react-combine-providers';
 
+import { uiLibraryConfig } from './theme';
+
 export const attachUiProviders = (manager: CombineProviders): void => {
-  manager.push(GluestackUIProvider, { config });
+  manager.push(GluestackUIProvider, { config: uiLibraryConfig });
 };
