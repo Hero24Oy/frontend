42c0f844a8cf4826b1cb417410672f7c2fad407d
Andrii Zinko <andrii.zinko@sysgears.com>
2023-10-24 01:14:41 +0300

    Update radio group component.

--- packages/ui/src/components/atoms/RadioGroup/index.tsx
+++ packages/ui/src/components/atoms/RadioGroup/index.tsx
@@ -1,30 +1,18 @@
 import { RadioGroup as GluestackRadioGroup } from '@gluestack-ui/themed';
-import React, { FC, ReactNode } from 'react';
-import { StyleSheet } from 'react-native';
+import React, { ComponentProps, FC, ReactNode } from 'react';
 
-interface Props {
+interface Props extends ComponentProps<typeof GluestackRadioGroup> {
   children: ReactNode | ReactNode[];
   onChange: (value: string) => void;
   value: string;
 }
 
 export const RadioGroup: FC<Props> = (props) => {
-  const { children, onChange, value } = props;
+  const { children, onChange, value, ...rest } = props;
 
   return (
-    <GluestackRadioGroup
-      value={value}
-      onChange={onChange}
-      style={styles.radioGroup}
-    >
+    <GluestackRadioGroup value={value} onChange={onChange} {...rest}>
       {children}
     </GluestackRadioGroup>
   );
 };
-
-const styles = StyleSheet.create({
-  radioGroup: {
-    display: 'flex',
-    gap: 6,
-  },
-});
