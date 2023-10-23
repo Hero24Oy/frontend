5939341dc07020985064517fc9a8a3cdd8700ace
Dmytro Laikov <dmytro.laikov@sysgears.com>
2023-10-24 00:12:15 +0300

    Fix change requests from @medreres and @dmytroipatov

--- packages/common/src/modules/Auth/components/forms/EmailSignInForm/index.tsx
+++ packages/common/src/modules/Auth/components/forms/EmailSignInForm/index.tsx
@@ -1,47 +1,11 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import { emailSignInFormValidationSchema } from 'core/validation';
-import { useEmailSignIn, useEmailSignUp } from 'modules/Auth/hooks';
 import React, { FC } from 'react';
-import { useForm } from 'react-hook-form';
 
 import { Button, Input } from '@hero24/ui';
 
-interface FormData {
-  email: string;
-  password: string;
-}
+import { useEmailSignInForm } from './hooks/useEmailSignInForm';
 
 export const EmailSignInForm: FC = () => {
-  const { control, handleSubmit } = useForm<FormData>({
-    resolver: yupResolver(emailSignInFormValidationSchema),
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-    mode: 'onSubmit',
-  });
-
-  const { signInWithEmail } = useEmailSignIn();
-  const { signUpWithEmail } = useEmailSignUp();
-
-  const signUp = async (data: FormData): Promise<void> => {
-    try {
-      await signUpWithEmail(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const signIn = async (data: FormData): Promise<void> => {
-    try {
-      await signInWithEmail(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const signInHandler = handleSubmit(signIn);
-  const signUpHandler = handleSubmit(signUp);
+  const { control, signInHandler } = useEmailSignInForm();
 
   return (
     <>
@@ -59,7 +23,6 @@ export const EmailSignInForm: FC = () => {
         name="password"
       />
       <Button onPress={signInHandler}>Sign in</Button>
-      <Button onPress={signUpHandler}>Sign up</Button>
     </>
   );
 };
