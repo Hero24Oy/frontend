ca37c374421f39afd25a24af247e88803ef3441a
Maksym Pryhoda <maxum23025@gmail.com>
2023-10-23 18:33:04 +0300

    Fix change requests.

--- packages/common/src/core/hooks/appInitialize/useInitializeApp.ts
+++ packages/common/src/core/hooks/appInitialize/useInitializeApp.ts
@@ -8,6 +8,7 @@ export const useInitializeApp: UseInitializeApp = () => {
   const { isUserLoading } = useInitializeUser();
 
   // * Check that every initializer finished loading
+  // TODO implement better logic for handling loading, for now is user loading is failed, this hook will return true
   const isAppInitialized = [isUserLoading].every(
     (isItemLoaded) => !isItemLoaded,
   );
