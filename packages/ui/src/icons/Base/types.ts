b9b6e2b21523f5a64b3f3fa298bfb4686b1d5acb
Andrii Zinko <andrii.zinko@sysgears.com>
2023-10-23 11:24:54 +0300

    Update types for base icon.

--- packages/ui/src/icons/Base/types.ts
+++ packages/ui/src/icons/Base/types.ts
@@ -2,8 +2,8 @@ import { Colors, IconSize } from 'configs';
 import { FC } from 'react';
 
 export interface Props {
-  color: Colors | string;
-  size: IconSize | number;
+  color?: Colors | string;
+  size?: IconSize | number;
 }
 
 export type BaseIcon = FC<Props>;
