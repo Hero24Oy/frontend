27744329554db149fe3c26f89202270320db69d3
Andrii Zinko <andrii.zinko@sysgears.com>
2023-10-24 01:14:15 +0300

    Update radio button component.

--- packages/ui/src/components/atoms/RadioButton/index.tsx
+++ packages/ui/src/components/atoms/RadioButton/index.tsx
@@ -2,47 +2,21 @@ import {
   Radio as GluestackRadio,
   RadioLabel as GluestackRadioLabel,
 } from '@gluestack-ui/themed';
-import { LineHeight } from 'configs';
 import React, { FC } from 'react';
-import { StyleSheet } from 'react-native';
-import { CommonStyles } from 'types';
-
-import { radioStyles } from './constants';
+import { StyleProp, ViewStyle } from 'react-native';
 
 interface Props {
   children: string;
   value: string;
-  style?: CommonStyles;
+  style?: StyleProp<ViewStyle>;
 }
 
 export const RadioButton: FC<Props> = (props) => {
   const { value, children, style } = props;
 
-  const { sx } = radioStyles;
-
   return (
-    <GluestackRadio value={value} style={[styles.button, style]} sx={sx}>
-      <GluestackRadioLabel style={styles.label}>{children}</GluestackRadioLabel>
+    <GluestackRadio value={value} style={style}>
+      <GluestackRadioLabel>{children}</GluestackRadioLabel>
     </GluestackRadio>
   );
 };
-
-const styles = StyleSheet.create({
-  button: {
-    display: 'flex',
-    borderWidth: 1,
-    borderRadius: 8,
-    gap: 4,
-    justifyContent: 'center',
-  },
-  label: {
-    fontSize: 14,
-    fontWeight: '400',
-    paddingHorizontal: 12,
-    paddingVertical: 8,
-    lineHeight: LineHeight.DEFAULT,
-  },
-  indicator: {
-    marginRight: 2,
-  },
-});
--- packages/ui/src/components/atoms/RadioButton/types/radioStyles.ts
+++
@@ -1,6 +0,0 @@
-import { SxProps } from '@gluestack-style/react/lib/typescript/types';
-import { StyleProp, ViewStyle } from 'react-native';
-
-export interface RadioStyles {
-  sx: Partial<SxProps<StyleProp<ViewStyle>>>;
-}
