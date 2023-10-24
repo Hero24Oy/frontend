511785935ade9e03aaef46526c531c1ee7344f29
Dmytro Laikov <dmytro.laikov@sysgears.com>
2023-10-25 01:01:19 +0300

    Fix paths

--- packages/ui/src/components/organisms/PhoneInput/components/SelectCountry.tsx
+++ packages/ui/src/components/organisms/PhoneInput/components/SelectCountry.tsx
@@ -1,9 +1,9 @@
+import { HStack } from 'components/atoms';
 import React, { ReactElement } from 'react';
 import { FieldValues } from 'react-hook-form';
 import { StyleSheet } from 'react-native';
 import CountryPicker, { CountryCode } from 'react-native-country-picker-modal';
 
-import { HStack } from '../../../atoms';
 import { PhoneInputProps } from '../types';
 
 import { CallingCode } from './components/CallingCode';
