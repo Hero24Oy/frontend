3319798b973fd7a614d8f870c1fd228d0c7553b6
Andrii Zinko <andrii.zinko@sysgears.com>
2023-10-27 17:31:21 +0300

    Update typings and provider.

--- packages/ui/src/theme/typings.d.ts
+++ packages/ui/src/theme/typings.d.ts
@@ -1,24 +1,8 @@
 import { ButtonSize, ButtonVariant, RadioVariant } from 'components';
 
-import {
-  colors,
-  fonts,
-  fontSizes,
-  fontWeights,
-  lineHeights,
-} from './constants';
 import { SxValues } from './types';
 
 declare module '@gluestack-ui/themed' {
-  interface UIConfig {
-    tokens: {
-      colors: Record<`${keyof typeof colors}`, string>;
-      fonts: Record<`${keyof typeof fonts}`, string>;
-      fontSizes: Record<`${keyof typeof fontSizes}`, number>;
-      fontWeights: Record<`${keyof typeof fontWeights}`, string>;
-      lineHeights: Record<`${keyof typeof lineHeights}`, number>;
-    };
-  }
   interface UIComponents {
     Button: {
       theme: {
