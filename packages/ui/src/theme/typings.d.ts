e03dd32fe7e8b8b05f966a5a9c6ffe8896069c85
Andrii Zinko <andrii.zinko@sysgears.com>
2023-10-27 10:59:34 +0300

    Add tokens into UIConfig.

--- packages/ui/src/theme/typings.d.ts
+++ packages/ui/src/theme/typings.d.ts
@@ -1,8 +1,18 @@
 import { ButtonSize, ButtonVariant, RadioVariant } from 'components';
 
+import { Color, Font, FontSize, FontWeight, LineHeight } from './constants';
 import { SxValues } from './types';
 
 declare module '@gluestack-ui/themed' {
+  interface UIConfig {
+    tokens: {
+      colors: Record<Color, string>;
+      fonts: Record<Font, string>;
+      fontSizes: Record<FontSize, number>;
+      fontWeights: Record<FontWeight, number>;
+      lineHeights: Record<LineHeight, number>;
+    };
+  }
   interface UIComponents {
     Button: {
       theme: {
