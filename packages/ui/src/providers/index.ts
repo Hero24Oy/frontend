6b14ef328a521b961a79de146f1ee28fa264a35a
Maksym Pryhoda <maxum23025@gmail.com>
2023-10-27 14:17:45 +0300

    Minor fixes.

--- packages/ui/src/providers/index.ts
+++ packages/ui/src/providers/index.ts
@@ -1,18 +1,11 @@
 import { config } from '@gluestack-ui/config';
-import { createConfig, GluestackUIProvider } from '@gluestack-ui/themed';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { CombineProviders } from 'react-combine-providers';
 
-import { Alert, AlertIcon } from '../theme/components/Alert';
-
-export const uiLibraryConfig = createConfig({
-  ...config,
-  components: {
-    ...config.components,
-    AlertIcon,
-    Alert,
-  },
-});
+// * Waiting for https://github.com/Helpdor/frontend/pull/29 to merge
+// TODO this should be added to provider in order for styles to work
+// import { Alert, AlertIcon } from '../theme/components/Alert';
 
 export const attachUiProviders = (manager: CombineProviders): void => {
-  manager.push(GluestackUIProvider, { config: uiLibraryConfig });
+  manager.push(GluestackUIProvider, { config });
 };
