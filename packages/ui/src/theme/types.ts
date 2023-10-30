85da2870bd0dbcaeb018c06436969d1d0c9d87e8
Andrii Zinko <andrii.zinko@sysgears.com>
2023-10-30 13:11:06 +0200

    Update ui library config.

--- packages/ui/src/theme/types.ts
+++ packages/ui/src/theme/types.ts
@@ -16,6 +16,7 @@ export type ComponentConfig = {
 export type ComponentTheme = {
   theme: Theme;
   componentConfig?: ComponentConfig;
+  props?: Record<string, unknown>;
 };
 
 export enum DescendantStyleName {
