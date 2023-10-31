1d4d99f9585042564db65cb330bc1ad4996e5c50
Andrii Zinko <andrii.zinko@sysgears.com>
2023-10-31 16:30:59 +0200

    Update config.

--- packages/ui/src/theme/config.ts
+++ packages/ui/src/theme/config.ts
@@ -1,5 +1,17 @@
+import { config as defaultConfig } from '@gluestack-ui/config';
+import omit from 'lodash/omit';
+
 import * as components from './components';
 
+const defaultComponents = omit(
+  defaultConfig.components,
+  Object.keys(components),
+);
+
 export const uiLibraryConfig = {
-  components,
+  ...defaultConfig,
+  components: {
+    ...defaultComponents,
+    ...components,
+  },
 };
