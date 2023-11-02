978d4b3c32ec58401b30dc66de9f2df4a6cb9d7c
Nikita Bereziuk <nikita.bereziuk@sysgears.com>
2023-11-02 15:05:52 +0200

    Add hello world screen

---
+++ apps/customer/src/app/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import { SafeAreaView, View } from '@hero24/ui';
+
+const Home: FC = () => {
+  return (
+    <SafeAreaView>
+      <View>Hello, world</View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
