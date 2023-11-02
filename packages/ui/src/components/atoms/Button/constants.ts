f033a023670d7746c8d677f721bdd044197a1d94
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-02 12:55:26 +0200

    Add todo and update comment.

--- packages/ui/src/components/atoms/Button/constants.ts
+++ packages/ui/src/components/atoms/Button/constants.ts
@@ -5,13 +5,13 @@ import { ButtonAction } from './types';
 import { Color, SxValues } from '$theme';
 
 // * This was implemented due to Gluestack bug,
-// * where styles don't apply to compound variant descendants
+// * where styles don't apply to compound variant descendants.
 // * Issue: https://github.com/gluestack/gluestack-style/issues/489.
-// * After fixing the bug, this styles constant can be removed.
+
+// TODO remove after fixing the Gluestack bug.
 export const buttonSxValue = {
   [ButtonAction.PRIMARY]: {},
   [ButtonAction.SECONDARY]: {
-    // eslint-disable-next-line @typescript-eslint/naming-convention -- this is temporary solution.
     ':active': {
       _text: {
         color: Color.DARK_GREY_00,
