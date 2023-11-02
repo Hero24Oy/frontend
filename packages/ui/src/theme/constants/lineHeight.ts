e5becf44fbed6bbed7c74dbfdc72b9aaccedd0ac
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-02 19:51:06 +0200

    Update line height constant.

--- packages/ui/src/theme/constants/lineHeight.ts
+++ packages/ui/src/theme/constants/lineHeight.ts
@@ -5,8 +5,8 @@ export const LineHeight: Record<TextSize, number> = {
   [TextSize.XXXL]: FontSize.XXXL * 1.5,
   [TextSize.XXL]: FontSize.XXL * 1.2,
   [TextSize.XL]: FontSize.XL * 1.3,
-  [TextSize.LG]: FontSize.LG * 1.3,
-  [TextSize.MD]: FontSize.MD * 1.5,
+  [TextSize.LG]: FontSize.LG * 1.5,
+  [TextSize.MD]: FontSize.MD * 1.3,
   [TextSize.SM]: FontSize.SM * 1.5,
   [TextSize.XS]: FontSize.XS * 1.5,
 };
