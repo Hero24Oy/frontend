3235784f5c1bc9b3bbb9fdd928a17a142ab18bc1
Ipatov Dmytro <139549233+dmytroipatov@users.noreply.github.com>
2023-11-03 12:41:52 +0200

    SC-2726: UI component header (#55)
    * create header component
    
    * change size to variant
    
    * opimize header
    
    * optimize styles
    
    * update component
    
    * delete override
    
    * update height calculation
    
    * Merge branch 'dev' of https://github.com/Helpdor/frontend into dmytroipatov/sc-2726/ui-component-header-component
    
    * fix comment

---
+++ packages/ui/src/components/molecules/Header/index.tsx
@@ -0,0 +1,67 @@
+import { FC } from 'react';
+import { StyleSheet } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { Heading, HStack, IconButton, View } from '$atoms';
+import { BaseIcon } from '$icons/base';
+import { Color } from '$theme';
+import { JsxElement } from '$types';
+
+interface Props {
+  goBackIcon: BaseIcon;
+  title: string;
+  goBack?: () => void;
+  rightComponent?: JsxElement;
+}
+
+export const Header: FC<Props> = (props) => {
+  const { goBack, title, goBackIcon, rightComponent = null } = props;
+
+  const styles = useStyles();
+
+  return (
+    <View style={styles.view}>
+      <HStack style={styles.container}>
+        <HStack style={{ ...styles.left, ...styles.sides }}>
+          <IconButton icon={goBackIcon} size="md" onPress={goBack} />
+        </HStack>
+        <Heading variant="H5">{title}</Heading>
+        <HStack style={{ ...styles.right, ...styles.sides }}>
+          {rightComponent}
+        </HStack>
+      </HStack>
+    </View>
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type -- StyleSheet doesn't have a correct return type
+export const useStyles = () => {
+  const insets = useSafeAreaInsets();
+
+  return StyleSheet.create({
+    view: {
+      borderBottomWidth: 0.2,
+      borderColor: Color.GREY_LIGHT_02,
+      marginTop: insets.top,
+    },
+    container: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      height: 74,
+    },
+    left: {
+      justifyContent: 'flex-start',
+    },
+    right: {
+      justifyContent: 'flex-end',
+    },
+    sides: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: 8,
+      gap: 10,
+      width: 107.5,
+    },
+  });
+};
