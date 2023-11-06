e046d18c1940512c947dd1ab5dbba30159b0a7c2
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-06 13:37:26 +0200

    Add apple sign in conditional rendering.

--- packages/common/src/modules/Auth/components/SignInProvidersButtons/index.tsx
+++ packages/common/src/modules/Auth/components/SignInProvidersButtons/index.tsx
@@ -1,15 +1,17 @@
 import { FC } from 'react';
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 import { Button, VStack } from '@hero24/ui';
 
 import { AppleSignInButton } from './AppleSignInButton';
 
 export const SignInProvidersButtons: FC = () => {
+  const isIOS = Platform.OS === 'ios';
+
   return (
     <VStack style={styles.container}>
       {/* TODO replace buttons labels with i18n call */}
-      <AppleSignInButton />
+      {isIOS ? <AppleSignInButton /> : null}
       <Button variant="outline" isDisabled>
         Continue with Google
       </Button>
