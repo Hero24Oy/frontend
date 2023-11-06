924d8f7e0b00e6c54b5026983df838817c6704b9
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-06 17:01:44 +0200

    Remove Array generic usage.

--- packages/ui/src/components/atoms/Hidden/types.ts
+++ packages/ui/src/components/atoms/Hidden/types.ts
@@ -2,5 +2,5 @@ import { PropsWithChildren } from 'react';
 import { Platform } from 'react-native';
 
 export type HiddenProps = PropsWithChildren<{
-  platforms?: Array<`${Platform['OS']}`>;
+  platforms?: `${Platform['OS']}`[];
 }>;
