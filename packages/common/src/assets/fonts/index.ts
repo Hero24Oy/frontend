3ac1d2d5ca94f81428b44dc6d037712b7cd59c82
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-07 20:36:43 +0200

    Add fonts.

---
+++ packages/common/src/assets/fonts/index.ts
@@ -0,0 +1,7 @@
+import OnestMediumPath from './Onest-Medium.ttf';
+import OnestRegularPath from './Onest-Regular.ttf';
+import OnestSemiBoldPath from './Onest-SemiBold.ttf';
+
+export const Onest400: string = require(OnestRegularPath);
+export const Onest500: string = require(OnestMediumPath);
+export const Onest600: string = require(OnestSemiBoldPath);
