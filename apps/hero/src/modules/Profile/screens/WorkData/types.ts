3c3d57e8a4e6a9d4a9125ca824c0b97069157a1a
dmytroipatov <dmytro.ipatov@sysgears.com>
2023-11-07 18:33:27 +0200

    delete unused type

--- apps/hero/src/modules/Profile/screens/WorkData/types.ts
+++ apps/hero/src/modules/Profile/screens/WorkData/types.ts
@@ -1,8 +1,3 @@
 import { CompanyCreation } from '$modules/Profile/stores';
 
 export type WorkData = CompanyCreation['workData'];
-
-export type ValidatedFieldsWorkData = Omit<
-  CompanyCreation['workData'],
-  'certificate' | 'insurance'
->;
