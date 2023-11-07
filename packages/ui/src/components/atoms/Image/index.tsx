85d19709632a87cbeb4ce0b42ec724b7562d1ae7
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-07 20:24:30 +0200

    Update Image component style property.

--- packages/ui/src/components/atoms/Image/index.tsx
+++ packages/ui/src/components/atoms/Image/index.tsx
@@ -1,13 +1,13 @@
 import { ComponentProps, FC } from 'react';
 
 import { StyledImage } from '$components/styled';
-import { CommonStyles } from '$types';
+import { LayoutStyles } from '$types';
 
 type PickedProps = Pick<ComponentProps<typeof StyledImage>, 'size'>;
 
 type Props = {
   source: string;
-  styles?: CommonStyles;
+  style?: LayoutStyles;
 } & PickedProps;
 
 export const Image: FC<Props> = (props) => <StyledImage {...props} />;
