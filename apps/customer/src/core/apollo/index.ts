55edc7dbfb7648a5b9f62c50d836be5785af059d
Maksym Pryhoda <maksym.pryhoda@sysgears.com>
2023-11-08 15:14:05 +0200

    Better apollo folder structure.

--- apps/customer/src/core/apollo/index.ts
+++ apps/customer/src/core/apollo/index.ts
@@ -1,11 +1,1 @@
-import { createApolloClient } from '@hero24/common';
-
-import { getAuthToken } from '../firebase';
-
-import { serverConfig } from '$configs';
-
-// TODO add type police
-export const apolloClient = createApolloClient({
-  getAuthToken,
-  serverUrl: serverConfig.apiUrl,
-});
+export * from './client';
