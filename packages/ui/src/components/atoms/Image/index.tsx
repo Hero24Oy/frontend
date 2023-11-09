3e3901ca74ce4c25b5de4537a577654325fb2f17
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-09 11:26:29 +0200

    Update Image component typing.

--- packages/ui/src/components/atoms/Image/index.tsx
+++ packages/ui/src/components/atoms/Image/index.tsx
@@ -1,13 +1,17 @@
 import { ComponentProps, FC } from 'react';
 
+import { ImageSource } from './types';
+
 import { StyledImage } from '$components/styled';
 import { LayoutStyles } from '$types';
 
 type PickedProps = Pick<ComponentProps<typeof StyledImage>, 'size'>;
 
 type Props = {
-  source: number | string;
+  source: ImageSource;
   style?: LayoutStyles;
 } & PickedProps;
 
 export const Image: FC<Props> = (props) => <StyledImage {...props} />;
+
+export * from './types';
