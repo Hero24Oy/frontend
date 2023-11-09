94f290f4148869f9f6bcad6ff3d76df87dcea01c
Maksym Pryhoda <maksym.pryhoda@sysgears.com>
2023-11-09 13:29:25 +0200

    Use auth hook.

--- packages/common/src/modules/Auth/components/EmailSignInForm/useLogic.ts
+++ packages/common/src/modules/Auth/components/EmailSignInForm/useLogic.ts
@@ -2,12 +2,17 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { emailSignInFormValidationSchema } from 'core/validation';
 import { useForm } from 'react-hook-form';
 
+// import { useAuthentication, useEmailSignIn } from '../../hooks';
+
 interface FormData {
   email: string;
   password: string;
 }
 
 export const useLogic = () => {
+  // const { signInWithCredentials } = useAuthentication();
+  // const { signInWithEmail } = useEmailSignIn({});
+
   const { control, handleSubmit } = useForm<FormData>({
     resolver: yupResolver(emailSignInFormValidationSchema),
     defaultValues: {
