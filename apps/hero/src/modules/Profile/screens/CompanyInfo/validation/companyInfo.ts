bcdc30093b02cc160f60f4fa693378ea94f28b49
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-10 15:45:33 +0200

    Remove shape from companyInfoSchema.

--- apps/hero/src/modules/Profile/screens/CompanyInfo/validation/companyInfo.ts
+++ apps/hero/src/modules/Profile/screens/CompanyInfo/validation/companyInfo.ts
@@ -2,12 +2,8 @@ import * as yup from 'yup';
 
 import { emailSchema, rangeSchema, ValidationHints } from '@hero24/common';
 
-import { CompanyCreation } from '$modules/Profile/stores';
-
-export const companyInfoSchema = yup
-  .object<CompanyCreation['companyInfo']>()
-  .shape({
-    email: emailSchema.required(ValidationHints.REQUIRED),
-    employees: rangeSchema.required(ValidationHints.REQUIRED),
-    name: yup.string().required(ValidationHints.REQUIRED),
-  });
+export const companyInfoSchema = yup.object({
+  email: emailSchema.required(ValidationHints.REQUIRED),
+  employees: rangeSchema.required(ValidationHints.REQUIRED),
+  name: yup.string().required(ValidationHints.REQUIRED),
+});
