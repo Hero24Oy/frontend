c387292626826c90fcadd3eb25f556aad26dfef4
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-10 11:31:32 +0200

    Replace company info screen.

---
+++ apps/hero/src/modules/Profile/screens/CompanyInfo/index.tsx
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import { FC } from 'react';
+import { StyleSheet } from 'react-native';
+
+import { Color, Heading, VStack } from '@hero24/ui';
+
+import { CompanyInfoForm } from '../../components';
+
+import { useLogic } from './useLogic';
+
+export const CompanyInfoScreen: FC = () => {
+  const { control, isValid, submitData } = useLogic();
+
+  return (
+    // TODO replace text in heading with i18n call
+    <>
+      <StatusBar style="dark" />
+
+      <VStack style={styles.vStack}>
+        <Heading variant="H2">Add company info</Heading>
+
+        <CompanyInfoForm
+          control={control}
+          isValid={isValid}
+          submitData={submitData}
+        />
+      </VStack>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  vStack: {
+    flex: 1,
+    gap: 24,
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    paddingBottom: 32,
+    backgroundColor: Color.WHITE_00,
+    position: 'relative',
+  },
+});
