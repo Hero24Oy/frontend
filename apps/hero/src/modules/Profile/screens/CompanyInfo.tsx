3ecaf9136025124cd7e0748af1400e5541d87403
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-10 21:54:11 +0200

    Update company info screen.

--- apps/hero/src/modules/Profile/screens/CompanyInfo.tsx
+++ apps/hero/src/modules/Profile/screens/CompanyInfo.tsx
@@ -1,14 +1,14 @@
 import { FC } from 'react';
 import { StyleSheet } from 'react-native';
 
-import { Color, Heading, VStack } from '@hero24/ui';
+import { Heading, VStack } from '@hero24/ui';
 
 import { CompanyInfoForm } from '../components';
 
 export const CompanyInfoScreen: FC = () => {
   return (
     // TODO replace text in heading with i18n call
-    <VStack style={styles.vStack}>
+    <VStack style={styles.container}>
       <Heading variant="H2">Add company info</Heading>
       <CompanyInfoForm />
     </VStack>
@@ -16,12 +16,11 @@ export const CompanyInfoScreen: FC = () => {
 };
 
 const styles = StyleSheet.create({
-  vStack: {
+  container: {
     flex: 1,
     gap: 24,
     paddingHorizontal: 16,
     paddingTop: 16,
     paddingBottom: 32,
-    backgroundColor: Color.WHITE_00,
   },
 });
