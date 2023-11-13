dd722e748860128552c7dc138083f62babf846d4
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-13 16:27:56 +0200

    Create bare screen.

---
+++ packages/common/src/modules/Profile/screens/Bio/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+type Props = {
+  title: string;
+};
+
+export const BioScreen: FC<Props> = () => {
+  return null;
+};
+
+export * from './useBioLogic';
--- packages/common/src/modules/index.ts
+++ packages/common/src/modules/index.ts
@@ -3,3 +3,4 @@ export * from './User';
 export * from './Auth';
 export * from './Chats';
 export * from './Common';
+export * from './Profile';
