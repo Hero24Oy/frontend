bb1732fa0c69e1169c1dff33e6208dc269583faa
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-14 15:23:06 +0200

    Update useStringifiedValue hook.

--- packages/ui/src/hooks/useStringifiedValue.ts
+++ packages/ui/src/hooks/useStringifiedValue.ts
@@ -1,11 +1,9 @@
-import { useMemo } from 'react';
-
 type ReturnType = {
   stringifiedValue: string;
 };
 
 export const useStringifiedValue = <Type>(value: Type): ReturnType => {
-  const stringifiedValue = useMemo(() => JSON.stringify(value), []);
+  const stringifiedValue = JSON.stringify(value);
 
   return {
     stringifiedValue,
