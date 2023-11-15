9f96ad7594a3dcb438f32174b8b9789f94f067f5
Maksym Pryhoda <maksym.pryhoda@sysgears.com>
2023-11-15 23:36:41 +0200

    Add ReCaptcha modal to customer app.

--- apps/customer/src/app/(auth)/_layout.tsx
+++ apps/customer/src/app/(auth)/_layout.tsx
@@ -1,24 +1,30 @@
-// import { Redirect } from 'expo-router';
+import { Redirect } from 'expo-router';
 import { FC } from 'react';
 
 import {
   HeadlessExpoStack,
-  // useFirebaseUser
+  ReCaptchaModal,
+  useFirebaseUser,
 } from '@hero24/common';
 
 const AppLayout: FC = () => {
-  // const { user } = useFirebaseUser();
+  const { user } = useFirebaseUser();
 
   // Only require authentication within the (app) group's layout as users
   // need to be able to access the (auth) group and sign in again.
-  // if (user) {
-  //   // On web, static rendering will stop here as the user is not authenticated
-  //   // in the headless Node process that the pages are rendered in.
-  //   return <Redirect href="/" />;
-  // }
+  if (user) {
+    // On web, static rendering will stop here as the user is not authenticated
+    // in the headless Node process that the pages are rendered in.
+    return <Redirect href="/" />;
+  }
 
   // This layout can be deferred because it's not the root layout.
-  return <HeadlessExpoStack />;
+  return (
+    <>
+      <ReCaptchaModal />
+      <HeadlessExpoStack />
+    </>
+  );
 };
 
 export default AppLayout;
