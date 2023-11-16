c84d6f8f4a45b01fb5b8c9772bd0d5ce80889982
Roman Borysenko <webdev2309@gmail.com>
2023-11-16 12:45:26 +0200

    Delete multiScreenProgressBar component

--- packages/common/src/core/components/MultiScreenProgressBar/types.ts
+++
@@ -1,6 +0,0 @@
-import { ScreensForms, Store } from '$core/store';
-
-export type MultiScreenProgressBarProps<Forms extends ScreensForms> = {
-  currentStoreValue: Store<Forms>;
-  initialStoreState: Forms;
-};
