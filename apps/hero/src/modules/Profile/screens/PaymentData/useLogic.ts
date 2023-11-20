551865bcb46bd48e96dcf92df9a4c3294e939e08
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-20 18:21:44 +0200

    Create useLogic hook and blank component.

---
+++ apps/hero/src/modules/Profile/screens/PaymentData/useLogic.ts
@@ -0,0 +1,58 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useRouter } from 'expo-router';
+import { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { useCreateMultiProgressBar } from '@hero24/common';
+
+import { getDefaultValues } from './utils';
+import { getPaymentDataSchema } from './validation';
+
+import {
+  getMultiProgressBarInitialState,
+  HeroType,
+  ProfileCreation,
+  useProfileCreationStore,
+} from '$modules/Profile/stores';
+
+export const useLogic = () => {
+  const {
+    setPaymentData,
+    welcome: { heroType },
+  } = useProfileCreationStore();
+
+  const router = useRouter();
+
+  const { control, getValues, setValue, formState } = useForm<
+    ProfileCreation['paymentData']
+  >({
+    resolver: yupResolver<ProfileCreation['paymentData']>(
+      getPaymentDataSchema(heroType!),
+    ),
+    defaultValues: getDefaultValues(heroType!),
+    mode: 'onChange',
+  });
+
+  const { multiScreenProgressBar } = useCreateMultiProgressBar({
+    initialState: getMultiProgressBarInitialState(),
+    progressBarInfo: {
+      formState,
+      getValues,
+      progressBarIndex: 3,
+    },
+  });
+
+  const submitData = useCallback(() => {
+    setPaymentData(getValues());
+    router.push('/');
+  }, []);
+
+  return {
+    multiScreenProgressBar,
+    control,
+    setValue,
+    isValid: formState.isValid,
+    submitData,
+    heroType: heroType ?? HeroType.INDIVIDUAL,
+  };
+};
