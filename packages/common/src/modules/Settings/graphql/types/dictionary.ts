7d76b50a750379c99f9db23df035e5219c4443a2
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-21 17:30:10 +0200

    Remove redundant type.

--- packages/common/src/modules/Settings/graphql/types/dictionary.ts
+++
@@ -1,9 +0,0 @@
-export enum SettingsDictionaryKeys {
-  ENGLISH = 'en',
-  FINNISH = 'fi',
-}
-
-export type SettingsDictionary = {
-  [SettingsDictionaryKeys.ENGLISH]: string[];
-  [SettingsDictionaryKeys.FINNISH]: string[];
-};
