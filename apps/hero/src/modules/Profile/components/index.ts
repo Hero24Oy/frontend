f6f70c201708665ee3705cc2f3f6f86d977af523
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-22 16:41:36 +0200

    Update address screen.

--- apps/hero/src/modules/Profile/components/index.ts
+++ apps/hero/src/modules/Profile/components/index.ts
@@ -1,6 +1,6 @@
 export * from './WelcomeHeading';
 export * from './HeroTypeCards';
 export * from './CompanyInfoForm';
-export * from './LanguagesSelect';
 export * from './BioForm';
 export * from './BioHeading';
+export * from './AddressForm';
